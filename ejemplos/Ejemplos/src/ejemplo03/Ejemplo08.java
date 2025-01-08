4fbdabe3235e66cf25f8c0c015d73215d3b23030
carloskes <cscorrea@utpl.edu.ec>
2025-01-08 12:36:39 -0500

    actualizado

--- ejemplos/Ejemplos/src/ejemplo03/Ejemplo08.java
+++ ejemplos/Ejemplos/src/ejemplo03/Ejemplo08.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package ejemplo03;
+
 import java.util.Scanner;
 
 /**
@@ -17,71 +18,61 @@ public class Ejemplo08 {
         String mensajeSuma;
         String mensajeSumaDos;
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Ingrese 1 o 2");
-       int valor1 = entrada.nextInt();
-       switch (valor1) {
-           case 1:
-               System.out.println("Ingrese el número a sumar");
-       int valor2 = entrada.nextInt();
+        System.out.println("Ingrese 1 para sumar o 2 para multiplicar");
+        int valor1 = entrada.nextInt();
+        switch (valor1) {
+            case 1:
+                System.out.println("Ingrese el número a sumar");
+                int valor2 = entrada.nextInt();
 
-               System.out.println("Ingrese el límite de tabla");
-       int valor3 = entrada.nextInt();
-               
-        mensajeSuma = obtenerTablaSumar(valor2, valor3);
-        System.out.printf("%s\n", mensajeSuma);
-           
-           break;
-           case 2:
-               System.out.println("Ingrese el número a sumar");
-       int valor4 = entrada.nextInt();
+                System.out.println("Ingrese el límite de tabla");
+                int valor3 = entrada.nextInt();
 
-               System.out.println("Ingrese el límite de tabla");
-       int valor5 = entrada.nextInt();
-       
-        mensajeSumaDos = obtenerTablaMultiplicar(valor4, valor5);
-        System.out.printf("%s\n", mensajeSumaDos);
-           
-           break;
-       }
-       
-        
-        
-        
-        
-        
-        
-         // se invoca al método 
-                                                       // obtenerTablaSumar
-                                                       // y el valor que 
-                                                       // devuelve se lo 
-                                                       // almacena  en mensajeSuma
-        
-        
-        
-        
+                mensajeSuma = obtenerTablaSumar(valor2, valor3);
+                System.out.printf("%s\n", mensajeSuma);
+
+                break;
+            case 2:
+                System.out.println("Ingrese el número a sumar");
+                int valor4 = entrada.nextInt();
+
+                System.out.println("Ingrese el límite de tabla");
+                int valor5 = entrada.nextInt();
+
+                mensajeSumaDos = obtenerTablaMultiplicar(valor4, valor5);
+                System.out.printf("%s\n", mensajeSumaDos);
+
+                break;
+        }
+
+        // se invoca al método 
+        // obtenerTablaSumar
+        // y el valor que 
+        // devuelve se lo 
+        // almacena  en mensajeSuma
     }
-        
-    public static String obtenerTablaSumar(int limite, int tabla){
+
+    public static String obtenerTablaSumar(int limite, int tabla) {
         int operacion;
         String cadena = "";
         for (int i = 1; i <= tabla; i++) {
             operacion = limite + i;
-            cadena = String.format("%s%d + %d = %d\n", cadena, limite, i, 
+            cadena = String.format("%s%d + %d = %d\n", cadena, limite, i,
                     operacion);
         }
-        return cadena;        
+        return cadena;
     }
-    
-    public static String obtenerTablaMultiplicar(int limite, int tabla){
+
+    public static String obtenerTablaMultiplicar(int limite, int tabla) {
         int operacion;
         String cadena = "";
         for (int i = 1; i <= tabla; i++) {
             operacion = tabla * i;
-            cadena = String.format("%s%d * %d = %d\n", cadena, limite, i, 
+            cadena = String.format("%s%d * %d = %d\n", cadena, limite, i,
                     operacion);
         }
         return cadena;
-        
+
     }
-    
+
 }
